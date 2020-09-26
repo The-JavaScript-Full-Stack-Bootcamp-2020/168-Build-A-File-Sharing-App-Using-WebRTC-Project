812b900e326bcfa6363ab09f2058d3ad2337b214
Evan Cope <copev313@gmail.com>
2020-09-26 13:36:24 -0400

    added sender and receiver

---
+++ sender.js
@@ -0,0 +1,26 @@
+const { PeerServer } = require("peer");
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    const peer = new Peer("sender", 
+                    {
+                        host: 'localhost',
+                        port: 9000,
+                        path: '/'
+                    });
+
+    const conn = peer.connect('receiver');
+
+    document.querySelector('input').onchange = function(event) {
+        const file = event.target.files[0];
+        const blob = new Blob(event.target.files, { type: file.type });
+    
+
+        conn.send({
+            file: blob,
+            filename: file.name,
+            filetype: file.type
+        });
+        
+    }
+});
